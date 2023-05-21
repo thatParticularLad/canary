6e81e9bc807f7a6d3003a4976450c6158b10cdeb
Adomas <sadasdasdas2312@gmail.com>
2023-05-21 14:27:35 +0300

    Feat: Add fonts and change styles slightly

--- contentScript.js
+++ contentScript.js
@@ -4,45 +4,7 @@ let posY;
 
 const body = document.querySelector("body");
 
-// // close dialog if it's open
-window.addEventListener("click", () => {
-  closeDialog(dialog);
-});
-
-const closeDialog = (dialog) => (dialog ? dialog.remove() : null);
-
-function displayDialog(result) {
-  const selection = window.getSelection();
-  if (!selection) {
-    console.log("Nothing was selected");
-    return;
-  }
-
-  const iconURL = chrome.runtime.getURL("assets/cool.png");
-  const dialogHtml = `
-  <img src=${iconURL} class="verdict-image" alt="" />
-  <p>${result}</p>
-  `;
-
-  dialog = document.querySelector("#response-dialog");
-  closeDialog(dialog);
-  dialog = document.createElement("dialog");
-  dialog.innerHTML = dialogHtml;
-  console.log("DIALOG", dialog);
-  dialog.id = "response-dialog";
-  dialog.open = true;
-  dialog.style = `position: absolute; top:${posY}px; left: ${posX}px;`;
-
-  document.body.appendChild(dialog);
-}
-
-// HANDLE RESPONSE DATA FROM MODEL
-chrome.runtime.onMessage.addListener((data) => {
-  displayDialog(data);
-  console.log("Message received!", data);
-});
-
-// HANDLE mousemove
+// Update mouse position post-click
 body.addEventListener("mouseup", handleMouseup);
 
 // Mouse up event handler function
@@ -65,3 +27,89 @@ function handleMouseup(event) {
     posY = event.clientY + 20 + scrollTop;
   }
 }
+
+function getImageName(label) {
+  switch (label) {
+    case "FAKE":
+      return "shocked.png";
+    case "REAL":
+      return "cool.png";
+    case "UNDECIDED":
+      return "thinking.png";
+    default:
+      return "thinking.png";
+  }
+}
+const rescale = (val, oMin, oMax, min, max) =>
+  ((val - oMin) * (max - min)) / (oMax - oMin) + min;
+
+function getDialogHTML(result) {
+  const MIN_CONFIDENCE = 75;
+
+  const resultObj = { label: "", confidence: 0 };
+  // Now a constant, but should be fetched form the user's settings
+  // confidence percent to assign a class to text
+  if (result <= 0.5) {
+    const rescaled = rescale(result, 0.5, 0, 0, 1) * 100;
+    new_value = rescaled.toFixed(2);
+    resultObj.confidence = new_value;
+    if (new_value >= MIN_CONFIDENCE) {
+      resultObj.label = "REAL";
+    } else {
+      resultObj.label = "UNDECIDED";
+    }
+  } else {
+    const rescaled = rescale(result, 0.51, 1, 0, 1) * 100;
+    new_value = rescaled.toFixed(2);
+    resultObj.confidence = new_value;
+    if (new_value >= MIN_CONFIDENCE) {
+      resultObj.label = "FAKE";
+    } else {
+      resultObj.label = "UNDECIDED";
+    }
+  }
+
+  const assetPath = getImageName(resultObj.label);
+  const iconURL = chrome.runtime.getURL(`assets/${assetPath}`);
+
+  const dialogHtml = `
+  <img src=${iconURL} alt="verdict-image" />
+  <p>We're <span>${resultObj.confidence}%</span> sure it's ${resultObj.label}</p>
+  `;
+
+  return dialogHtml;
+}
+
+// // close dialog if it's open
+window.addEventListener("click", () => {
+  closeDialog(dialog);
+});
+
+const closeDialog = (dialog) => (dialog ? dialog.remove() : null);
+
+function displayDialog(result) {
+  const selection = window.getSelection();
+  if (!selection) {
+    console.log("Nothing was selected");
+    return;
+  }
+
+  const dialogHtml = getDialogHTML(result);
+
+  dialog = document.querySelector("#response-dialog");
+  closeDialog(dialog);
+  dialog = document.createElement("dialog");
+  dialog.innerHTML = dialogHtml;
+  console.log("DIALOG", dialog);
+  dialog.id = "response-dialog";
+  dialog.open = true;
+  dialog.style = `position: absolute; top:${posY}px; left: ${posX}px;`;
+
+  document.body.appendChild(dialog);
+}
+
+// HANDLE RESPONSE DATA FROM MODEL
+chrome.runtime.onMessage.addListener((data) => {
+  displayDialog(data);
+  console.log("Message received!", data);
+});
